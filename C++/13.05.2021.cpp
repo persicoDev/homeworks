a7a1c100f9e04f63c7c3a986d97cbb142b346334
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-05-13 11:41:04 +0200

    adding more homeworks.

---
+++ C++/13.05.2021.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct ufficio {
+    int codice;
+    string descrizione, nome_impiegato;
+    struct uffucio *next;
+};
+
+class gestoreUfficio {
+
+    
+    public:
+
+    gestoreUfficio() {
+        
+    }
+};
