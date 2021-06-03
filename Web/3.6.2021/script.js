646955fa73214c03753b3c4e90520a73dea780cd
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-06-03 23:59:56 +0200

    fixed least common multiple.

--- Web/3.6.2021/script.js
+++ Web/3.6.2021/script.js
@@ -1,9 +1,10 @@
 function getMcm() {
-    const valueNum = prompt('inserisci il numero dei numeri');
+    const index_num = prompt('inserisci il numero dei numeri');
     let num = [];
     
-    for(valueNum in num) 
+    for(let i = 0; i < index_num; i ++) 
         num.push(prompt('inserisci il numero'));
-
-    
-}
+    const greatest_common_divisor = (a, b) => (a ? greatest_common_divisor(b % a, a) : b);
+    const least_common_multiple = (a, b) => (a * b) / greatest_common_divisor(a, b);
+    alert(`il minimo comune multiplo è: ${num.reduce(least_common_multiple)}`);
+ }
